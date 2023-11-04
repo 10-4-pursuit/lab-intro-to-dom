aaadccab9a311f959933d337e4ee96f84507ddf6
ada lovelace <williambookman@pursuit.org>
2023-11-04 16:55:47 -0400

    tasks completed with comment

--- main.js
+++ main.js
@@ -1,51 +1,63 @@
-// Add a class of "featured" to the first section element on the page.
+//this adds a class of "featured" to the first section element on the page.
 const featured = document.querySelector("section");
 featured.classList.add("featured");
 
-//Create the following article element with JavaScript and add at the end of the section.posts element.
+//creates new article element
 const newArticleElement = document.createElement("article");
 
-const newTitleElement = document.createElement('h3');
-newTitleElement.textContent = "Stop Planning"
-const newImageElement = document.createElement('img');
+//creates new title element
+const newTitleElement = document.createElement("h3");
+newTitleElement.textContent = "Stop Planning";
 
-
-
-const newContentElement = document.createElement('p');
-
-newContentElement.innerText = `You -- yes you! You're an over-planner, I can tell. It's time to stop
-planning so much and instead focusing on relaxing. Taking a break at all is
-so stressful these days; why add to the stress by overworking yourself? `;
-
-newArticleElement.appendChild(newImageElement);
-newArticleElement.appendChild(newTitleElement);
-newArticleElement.appendChild(newContentElement);
-
-
-const sectionPostsElement = document.querySelector('section.posts');
-sectionPostsElement.appendChild(newArticleElement);
-
-document.querySelector("section.posts").appendChild(newArticleElement);
-
-
-newImageElement.setAttribute ( 
+//create new image element with attributes
+const newImageElement = document.createElement("img");
+newImageElement.setAttribute(
     "src",
     "/images/paul-gilmore-unsplash.jpg",
     "alt",
     "Image of a mountain in front of a lake.",
     "h3",
     "Stop Planning"
-)
+  );
+
+//create new content element with text
+const newContentElement = document.createElement("p");
+newContentElement.innerText = `You -- yes you! You're an over-planner, I can tell. It's time to stop
+planning so much and instead focusing on relaxing. Taking a break at all is
+so stressful these days; why add to the stress by overworking yourself? `;
 
 
-const postsSection = document.querySelector('section.posts')
+//append new elements to new article
+newArticleElement.appendChild(newImageElement);
+newArticleElement.appendChild(newTitleElement);
+newArticleElement.appendChild(newContentElement);
 
+//append article to posts section
+const sectionPostsElement = document.querySelector("section.posts");
+sectionPostsElement.appendChild(newArticleElement);
+
+//this switches first article with the second article under post
+const postsSection = document.querySelector("section.posts");
 const firstArticle = postsSection.firstElementChild;
 const secondArticle = firstArticle.nextElementSibling;
+postsSection.insertBefore(secondArticle, firstArticle);
 
-// postsSection.removeChild(firstArticle)
+//removes all post link from header
+const header = document.querySelector("header a");
+header.parentNode.removeChild(header);
 
-//this insert the second article after  
-postsSection.insertBefore(secondArticle, firstArticle)
+//removes read time from featured article
+const featuredPost = document.querySelector(".featured article");
+const readTimeSpan = featuredPost.querySelector("span");
+readTimeSpan.parentNode.removeChild(readTimeSpan);
 
+//removes last post
+const lastPost = postsSection.childNodes[5];
+lastPost.remove(lastPost);
 
+//Remove all titles from all non-featured post
+postsSection.querySelectorAll("h3");
+const allTitles = postsSection.querySelectorAll("h3");
+allTitles.forEach((title) => {
+  title.parentNode.removeChild(title);
+});
