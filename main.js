03b92af513fc432b8f18006a9f155c45148be0b2
MoniqueC10-4 <monique.charles13@gmail.com>
2023-12-04 00:41:29 -0500

    submitting intro to dom lab

---
+++ main.js
@@ -0,0 +1,11 @@
+function addClassFeatured() {
+    const element = document.querySelector("section");
+    element.classList.add("featured"); 
+}
+
+function createElementAndAppend() {
+    const element = document.createElement("article");
+    Node.append(section-posts);
+}
+createElementAndAppend()
+addClassFeatured()
